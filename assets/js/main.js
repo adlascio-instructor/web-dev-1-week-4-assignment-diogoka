eea232164468f2729789613c33e0952f027ebd45
diogoka <diogokalmeida@gmail.com>
2023-03-04 15:41:43 -0800

    Adding ALT in JS functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,30 +6,30 @@ const changeBorder = (element, newBorder) => {
     element.style.border = newBorder
 }
 
-const changeSource = (element, newSource) => {
+const changeSource = (element, newSource, newAlt) => {
     element.src=newSource
+    element.alt=newAlt
 }
 
 inputEmail.addEventListener("focusin", () =>{
-    changeSource(document.querySelector("#emailIcon"),"./assets/images/mail-focus.svg")
+    changeSource(document.querySelector("#emailIcon"),"./assets/images/mail-focus.svg","emailIconFocused")
     changeBorder(document.querySelector(".email"), "1px solid #ffc632");
 })
 
 inputEmail.addEventListener("focusout", () =>{
+    changeSource(document.querySelector("#emailIcon"),"./assets/images/mail.svg","emailIcon")
     changeBorder(document.querySelector(".email"),"1px solid #afb6c2");
-    changeSource(document.querySelector("#emailIcon"),"./assets/images/mail.svg")
 })
 
 inputPassword.addEventListener("focusin", () =>{
+    changeSource(document.querySelector("#passwordIcon"),"./assets/images/lock-focus.svg", "lockIconFocused")
     changeBorder(document.querySelector(".password"),"1px solid #ffc632")
-    changeSource(document.querySelector("#passwordIcon"),"./assets/images/lock-focus.svg")
 })
 
 inputPassword.addEventListener("focusout", () =>{
     const pw = inputPassword.value;
     validatePassword(pw);  
 })
-
 
 revealPassword.addEventListener("click", () => {
     const inputPassword = document.querySelector('#password');
@@ -40,16 +40,15 @@ revealPassword.addEventListener("click", () => {
     }
 })
 
-
 const validatePassword = (input) => {
     const validation = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
     if(input.match(validation)){
         changeBorder(document.querySelector(".password"),"1px solid #afb6c2")
-        changeSource(document.querySelector("#passwordIcon"),"./assets/images/lock.svg")
+        changeSource(document.querySelector("#passwordIcon"),"./assets/images/lock.svg","lockIcon")
     } else {
         alert("The password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters")
         changeBorder(document.querySelector(".password"),"1px solid #afb6c2")
-        changeSource(document.querySelector("#passwordIcon"),"./assets/images/lock.svg")
+        changeSource(document.querySelector("#passwordIcon"),"./assets/images/lock.svg", "lockIcon")
     }
 }
 
